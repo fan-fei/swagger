67a1255bd2d778cd464d723c81e46506dc46f4ea
樊飞 <fanfei@ctv.org.cn>
2018-09-20 14:06:34 +0800

    update

--- swagger/src/main/java/swagger/UserController.java
+++ swagger/src/main/java/swagger/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 
 @RestController
@@ -14,7 +15,7 @@ import io.swagger.annotations.ApiOperation;
 public class UserController {
 
     @ApiOperation(value = "输入用户ID并返回用户ID", notes = "暂无备注")
-    @ApiImplicitParam(name = "id", value = "用户ID", paramType = "path", dataType = "integer", required = false)
+    @ApiImplicitParams({ @ApiImplicitParam(name = "id", value = "用户ID", paramType = "path", dataType = "integer", required = false) })
     @RequestMapping(value = "/users/{id}", method = RequestMethod.GET)
     public String name(@PathVariable String id) {
         return id;
